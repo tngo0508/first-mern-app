1bb54b3ce7d4d2992d42f5d455ba388ada651b0b
Thomas Ngo <tngo0508@gmai.com>
2020-06-09 19:25:02 -0700

    integrating user auth and protected routes

--- server/express.js
+++ server/express.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import helmet from "helmet";
 import Template from "./../template";
 import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
@@ -17,9 +18,19 @@ app.use(helmet());
 app.use(cors());
 
 app.use("/", userRoutes);
+app.use("/", authRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send(Template());
+});
+
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ": " + err.message });
+    console.log(err);
+  }
 });
 
 export default app;
