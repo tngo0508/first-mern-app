61bfa3031b75ab920a1b5cbd2f8f4056cb5f4c06
Thomas Ngo <tngo0508@gmai.com>
2020-06-11 21:36:22 -0700

    implementing basic server-side rendering

--- client/core/Home.js
+++ client/core/Home.js
@@ -5,13 +5,13 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import unicornbikeImg from "./../assets/images/unicornbike.jpg";
-import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   card: {
     maxWidth: 600,
     margin: "auto",
     marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
   },
   title: {
     padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(
@@ -21,6 +21,15 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     minHeight: 400,
+  },
+  credit: {
+    padding: 10,
+    textAlign: "right",
+    backgroundColor: "#ededed",
+    borderBottom: "1px solid #d0d0d0",
+    "& a": {
+      color: "#3f4771",
+    },
   },
 }));
 
@@ -34,10 +43,26 @@ export default function Home() {
       <CardMedia
         className={classes.media}
         image={unicornbikeImg}
-        title="Unicorn Bicyle"
-      ></CardMedia>
+        title="Unicorn Bicycle"
+      />
+      <Typography
+        variant="body2"
+        component="p"
+        className={classes.credit}
+        color="textSecondary"
+      >
+        Photo by{" "}
+        <a
+          href="https://unsplash.com/@boudewijn_huysmans"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Boudewijn Huysmans
+        </a>{" "}
+        on Unsplash
+      </Typography>
       <CardContent>
-        <Typography variant="body2" component="p">
+        <Typography variant="body1" component="p">
           Welcome to the MERN Skeleton home page.
         </Typography>
       </CardContent>
